f81b79e0db1c1f3a292e4e6ac29ff9b0857f2d09
PiotrSierant <piotr.sierant96@gmail.com>
2022-08-31 14:10:06 +0200

    alias

---
+++ rozdzial02/alias.js
@@ -0,0 +1,6 @@
+var score = 50;
+console.log(score);
+score += 10;
+console.log(score);
+score = 20;
+console.log(score);
---
+++ rozdzial02/alias.ts
@@ -0,0 +1,9 @@
+type Point = 20 | 40 | 50 | 60 | 80 | 100;
+let score: Point = 50;
+console.log(score);
+
+score += 10;
+console.log(score);
+
+score = 20;
+console.log(score);
