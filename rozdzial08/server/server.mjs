efec6dd496bdede189fd6cabda581828f5d8ae71
PiotrSierant <piotr.sierant96@gmail.com>
2022-10-24 08:53:09 +0200

    trasowanie

--- rozdzial08/server/server.mjs
+++ rozdzial08/server/server.mjs
@@ -1,7 +1,24 @@
 import http from 'http';
 const server = http.createServer((req, res) => {
-    console.log(req)
-    res.end('Witaj świecie!');
+    if (req.url === "/") {
+        res.end("Witaj, świecie!");
+    } else if (req.url === "/a") {
+        res.end("Witaj na trasie A");
+    } else if (req.url === "/b") {
+        res.end("Witaj na trasie B");
+    } else if (req.url === "/c" && req.method === "POST") {
+        let body = [];
+        req.on("data", (chunk) => {
+            body.push(chunk);
+        });
+        req.on("end", () => {
+            const params = Buffer.concat(body);
+            console.log("treść", params.toString());
+            res.end(`Przesłane parametry: ${params.toString()}`);
+        });
+    } else {
+        res.end("Do zobaczenia");
+    }
 });
 
 const port = 8000;
